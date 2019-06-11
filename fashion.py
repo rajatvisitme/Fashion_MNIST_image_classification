f25ba3a8f4d3473b8754da8aa8690794b9a06a13
Rajat Agrawal <46888236+rajatvisitme@users.noreply.github.com>
2019-06-11 11:12:23 +0530

    Update fashion.py

--- fashion.py
+++ fashion.py
@@ -1,32 +1,39 @@
 import tensorflow as tf
 from tensorflow import keras
-
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Checking version of Tensorflow, code used in this program works better with tensorflow version 1.9 or higher.
 print(tf.__version__)
 
+#Fetching Fashion MNIST dataset
 fashion_mnist = keras.datasets.fashion_mnist
 
+#Loading the dataset
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+#Labels 0-9 for cloth groups
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+'''
+#Checking Shapes and Sizes of training and testing datasets
 print(train_images.shape)
 print(len(train_labels))
 print(train_labels)
 print(test_images.shape)
 print(len(test_labels))
-
+'''
+#Plotting the first image from training dataset
 plt.figure()
 plt.imshow(train_images[0])
 plt.colorbar()
 plt.grid(False)
 
+#Preprocessing the data to scale of 0 to 1
 train_images = train_images / 255.0
-
 test_images = test_images / 255.0
 
+#Display the first 25 images from the training set and display the class name below each image
 plt.figure(figsize=(10, 10))
 for i in range(25):
     plt.subplot(5, 5, i + 1)
@@ -36,22 +43,27 @@ for i in range(25):
     plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
 
+#Building the model and setting up the layers
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
+#Compile the model before training, i.e. setting-up Optimizer, Loss function etc.
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
+#Train the model
 model.fit(train_images, train_labels, epochs=5)
 
+#Evaluate accuracy
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 
 print('Test accuracy:', test_acc)
 
+#Making Predictions
 predictions = model.predict(test_images)
 print(predictions[0])
 print(np.argmax(predictions[0]))
